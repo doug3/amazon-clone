f1c6fd3cd662e7c492bc9dd867ea31647f98f77e
doug3 <doug.corcoran@hotmail.com>
2025-09-08 09:07:53 -0400

    Third commit

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,8 +3,38 @@ import Product from './Product'
 
 function ProductFeed({ products }) {
   return (
-    <div>
-      {products.map(({ id, title, price, description, category, image }) => (
+    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-44 mx-auto'>
+
+      {products.slice(0, 4).map(({ id, title, price, description, category, image }) => (
+        <div className='' key={id}>
+          <Product
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        </div>
+      ))}
+
+      <img className="md:col-span-full" src="https://links.papareact.com/dyz" alt="" />
+
+    <div className='md:col-span-2'>
+      {products.slice(4, 5).map(({ id, title, price, description, category, image }) => (
+        <div key={id}>
+          <Product
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        </div>
+      ))}
+      </div>
+      {products.slice(5, products.length).map(({ id, title, price, description, category, image }) => (
         <div key={id}>
           <Product
             id={id}
